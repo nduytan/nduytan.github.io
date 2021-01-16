4b1bd5d91f8aff81819c53f4f762e60a2e1614f2
nduytan <nguyenduytan.2294@gmail.com>
2021-01-17 03:03:35 +0700

    Update code.py

--- code.py
+++ code.py
@@ -1,1 +1,15 @@
+def calc(a, b, o):
+  d = {'+': a + b,
+       '-': a - b,
+       '*': a * b,
+       '/': a / b,
+       '%': a % b
+       }
+  return f"({a}{o}{b})=({d[o]})"
 
+
+a = float(input('Enter first number : '))
+b = float(input('Enter second number : '))
+o = input('Enter the Operator (+,-,*,/,%) : ')
+
+document <= calc(a, b, o)
