93e330755fa2ac8dc13f2fb078b0d0b8a735165f
nduytan <nguyenduytan.2294@gmail.com>
2021-02-26 01:14:51 +0700

    Update code.py

--- code.py
+++ code.py
@@ -1,17 +1,36 @@
-from browser import document
-
-def calc(a, b, o):
-  d = {'+': a + b,
-       '-': a - b,
-       '*': a * b,
-       '/': a / b,
-       '%': a % b
-       }
-  return f"({a}{o}{b})=({d[o]})"
+from browser import document, ajax, bind
+from browser.widgets.dialog import InfoDialog
 
 
-a = float(input('Enter first number : '))
-b = float(input('Enter second number : '))
-o = input('Enter the Operator (+,-,*,/,%) : ')
+# bdfb5671e56189cb2073ef6a0b99a523 106.67417764663696, 11.063078488967657
+# api.openweathermap.org/data/2.5/weather?lat=11.063078488967657&lon=106.67417764663696&appid=bdfb5671e56189cb2073ef6a0b99a523
+lat = ""
+lng = ""
+api = "bdfb5671e56189cb2073ef6a0b99a523"
+url = "http://api.openweathermap.org/data/2.5/weather"
 
-document <= calc(a, b, o)
+def on_complete(req):
+   if req.status == 200 or req.status == 0:
+       InfoDialog("Hello", req.text) 
+       document['lat'] <= "blah "
+       
+   else:
+       InfoDialog("Error", req.text) 
+
+def test():
+    InfoDialog("Error", "asdasd") 
+
+       
+
+
+def getData(aa):
+    url = document['url'].value
+    req = ajax.Ajax()
+    req.bind('complete', on_complete)
+    # send a POST request to the url
+    req.open('GET', url, True)
+    req.set_header('content-type', 'application/x-www-form-urlencoded')
+    # send data as a dictionary
+    req.send({'x': 0, 'y': 1})
+
+document["map"].bind("click", getData)
